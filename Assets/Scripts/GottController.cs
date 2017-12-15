e9765c16859b110638fea1fe837b563b290c9482
pupi130396 <pupi130396@gmail.com>
2017-12-15 22:28:24 +0100

    Controller für Gott

---
+++ Assets/Scripts/GottController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gottcontroller1 : MonoBehaviour {
+
+
+
+    public float speed = 0.1f;
+    public void FixedUpdate()
+    {
+        //Move right
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+
+        //Move left
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+
+        //Move down
+        if (Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+
+        //Move up
+        if ((Input.GetKey(KeyCode.UpArrow))|| Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        }
+    }
+}
