43b5c990c8a74620fadd2f3355cfaf0d2435c962
Stefano Probst <stefano.probst@sensolligent.com>
2017-12-15 21:34:34 +0100

    Add network code & graphics folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour
+{
 	// Update is called once per frame
 	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         var x = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
         var z = Input.GetAxis("Horizontal") * Time.deltaTime * -150.0f;
 
