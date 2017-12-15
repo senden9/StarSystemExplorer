dd1102082bc246593937bd2be20b5a21afb3f93f
Stefano Probst <stefano.probst@sensolligent.com>
2017-12-15 21:14:16 +0100

    Add first network tests + player controller script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Horizontal") * Time.deltaTime * -150.0f;
+
+        transform.Translate(0, x, 0);
+        transform.Rotate(0, 0, z);
+    }
+}
