637534c6fd0bb54eac5049cd244ff24f256ba320
Ferdinand <fkoeppen@edu.auu.at>
2017-12-16 00:33:30 +0100

    Added planet and scailng

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Planet : MonoBehaviour {
+public class Planet : MonoBehaviour
+{
 
+	public float scale = 1.0f;	
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		this.transform.localScale = Vector3.one * scale;
 	}
 	
 	// Update is called once per frame
