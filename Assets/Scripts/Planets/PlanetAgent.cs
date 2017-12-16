3a5526f3f7a8c125bcb6d5518f1c3dcfaa406e9d
Ferdinand Köppen <fkoeppen@edu.auu.at>
2017-12-16 15:06:29 +0100

    Abstract class

--- Assets/Scripts/Planets/PlanetAgent.cs
+++ Assets/Scripts/Planets/PlanetAgent.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetAgent : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public abstract class PlanetAgent 
+{
 }
