1faeea29a4817e5363f61104aa8b2ea31b5d819b
LangenbachJulian <34583078+LangenbachJulian@users.noreply.github.com>
2017-12-16 18:34:54 +0100

    Ability Cooldowns for UI

---
+++ Assets/Scripts/SkillBarController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillBarController : MonoBehaviour
+{
+
+    public Image ImageCooldown;
+    bool isCooldown;
+    public Text cooldownCounter;
+    public float cooldown;
+    private float cooldownNumber;
+    public string key;
+
+    void Start()
+    {
+        cooldownNumber = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCooldown == false)
+        {
+            cooldownNumber = cooldown;
+            cooldownCounter.text = "";
+        }
+        if (Input.GetKeyDown(key))
+        {
+            isCooldown = true;
+        }
+
+        if (isCooldown)
+        {
+            ImageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if (ImageCooldown.fillAmount >= 1)
+            {
+                ImageCooldown.fillAmount = 0;
+                isCooldown = false;
+
+            }
+            cooldownNumber -= Time.deltaTime;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        cooldownCounter.text = cooldownNumber.ToString();
+    }
+}
