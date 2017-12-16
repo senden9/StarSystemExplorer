59f9fccd88681569bc58186507cafb2a0f8d2b3d
Stefano Probst <stefano.probst@sensolligent.com>
2017-12-16 18:34:05 +0100

    Add a health script to the spacecraft. Damage hit per astroid.

--- Assets/Scripts/Asteorid.cs
+++ Assets/Scripts/Asteorid.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteorid : MonoBehaviour {
+    [Range(0,100)]
+    public int damage = 10;
+
     void OnCollisionEnter2D(Collision2D coll) {
         if (coll.gameObject.GetComponent<Asteorid>() == null) {
             Destroy(gameObject);
+        }
+
+        PlayerHealth health = coll.gameObject.GetComponent<PlayerHealth>();
+        if (health != null)
+        {
+            health.TakeDamage(damage);
         }
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!", gameObject);
+        }
+    }
+}
