ed082954c2d6d5a3dab03f1e2a64e4a0674c043a
Unknown <maoffermanns@edu.aau.at>
2017-12-17 11:05:47 +0100

    Added placeholder audio and let backgroundmusic play
    Added backgroundmusic to the playerui tab (have to exchange for our own music)

---
+++ Assets/Scripts/playBackgroundaudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playBackgroundaudio : MonoBehaviour {
+
+
+    private AudioSource backgroundmusic;
+	// Use this for initialization
+	void Awake () {
+        backgroundmusic = GetComponent<AudioSource>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
