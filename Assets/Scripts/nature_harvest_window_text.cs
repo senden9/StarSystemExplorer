98c6fd43c03b0a534a9920446c69f61ecf321072
Unknown <maoffermanns@edu.aau.at>
2017-12-17 14:06:59 +0100

    changes to the nature gui

--- Assets/Scripts/nature_harvest_window_text.cs
+++ Assets/Scripts/nature_harvest_window_text.cs
@@ -5,22 +5,25 @@ using UnityEngine.UI;
 
 public class nature_harvest_window_text : MonoBehaviour {
     public Text Harvesttext;
-    bool bla=true;
+    bool bla;
     // Use this for initialization
     void Start () {
+        bla = true;
         Harvesttext = GetComponent<Text>();
     }
 
     public void Buttonpressedtest1()
     {
+        bla = !bla;
         if (bla)
         {
             setHarvestGui(14, bla);
-            bla = !bla;
         }
+        
     }
 
     public void setHarvestGui(int Resources_available,bool harvesting) {
+        Debug.Log(harvesting);
         string guitext = "Resources available:" + Resources_available.ToString()+"\n";
         if (!harvesting) {
             guitext = guitext + "To start harvesting: H";
